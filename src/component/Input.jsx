a5b0a400053df6c658f061ae4fa9270d70016e8c
amirah <mirez.data@gmail.com>
2024-03-05 11:59:52 +0800

    add changes

---
+++ src/component/Input.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Input(){
+    return(
+        <div>
+            <h1>Pregnancy BMI Calculator</h1>
+            <p>Welcome to our Pregnancy BMI Calculator app! Easily track your BMI and ideal weight during pregnancy. 
+               Get expert tips for a healthy journey. Remember, consult your healthcare provider for personalized advice
+            </p>
+            <div>
+                <h2>BMI Calculator</h2>
+                <div>
+                    <h3>Your height</h3>
+                    <input type="number" name="" id="" />
+                    <input type="number" name="" id="" />
+                    <input type="number" name="" id="" />
+                </div>
+                <div>
+                    <h3>Your Pre-pregnancy weight</h3>
+                    <input type="number" name="" id="" />
+                    <input type="number" name="" id="" />
+                </div>
+                <div>
+                    <h3>Your weight right now</h3>
+                    <input type="number" name="" id="" />
+                    <input type="number" name="" id="" />
+                </div>
+            </div>
+        </div>
+    )
+}
