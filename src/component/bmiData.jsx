c21632dd8fd895cf56322666f2fd7a67372b231d
amirah <mirez.data@gmail.com>
2024-03-09 23:12:04 +0800

    commit changes

---
+++ src/component/bmiData.jsx
@@ -0,0 +1,229 @@
+export default {
+    "success": true, 
+    "data": {
+        "bmi_data": [
+            {
+                "id": "00001", 
+                "BMI": "15", 
+                "Category" : "Very severely underweight", 
+                "Description": "Don't worry too much though as most people who have a low BMI in pregnancy are fine and have healthy pregnancies and babies. But you may be referred for extra scans to check on your baby's growth and development. Your midwife or specialist will talk to you about what your target weight gain in pregnancy should be."
+
+            }, 
+            {
+                "id": "00002", 
+                "BMI": "16", 
+                "Category" : "Severly underweight", 
+                "Description": "Don't worry too much though as most people who have a low BMI in pregnancy are fine and have healthy pregnancies and babies. But you may be referred for extra scans to check on your baby's growth and development. Your midwife or specialist will talk to you about what your target weight gain in pregnancy should be."
+
+            }, 
+            {
+                "id": "00003", 
+                "BMI": "17", 
+                "Category" : "Underweight", 
+                "Description": "Don't worry too much though as most people who have a low BMI in pregnancy are fine and have healthy pregnancies and babies. But you may be referred for extra scans to check on your baby's growth and development. Your midwife or specialist will talk to you about what your target weight gain in pregnancy should be."
+
+            },  
+            {
+                "id": "00004", 
+                "BMI": "18", 
+                "Category" : "Underweight range", 
+                "Description": "Don't worry too much though as most people who have a low BMI in pregnancy are fine and have healthy pregnancies and babies. But you may be referred for extra scans to check on your baby's growth and development. Your midwife or specialist will talk to you about what your target weight gain in pregnancy should be."
+
+            },  
+            {
+                "id": "00005", 
+                "BMI": "19", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00006", 
+                "BMI": "20", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00007", 
+                "BMI": "21", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00008", 
+                "BMI": "22", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00009", 
+                "BMI": "23", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00010", 
+                "BMI": "24", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00011", 
+                "BMI": "25", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00012", 
+                "BMI": "26", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00013", 
+                "BMI": "27", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00014", 
+                "BMI": "28", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00015", 
+                "BMI": "29", 
+                "Category" : "Normal BMI"
+
+            },  
+            {
+                "id": "00016", 
+                "BMI": "30", 
+                "Category" : "Obese", 
+                "Description": "Try not to be offended if anyone involved with your healthcare uses the word obese to describe your weight. Remember that no-one is judging you. The medical experts looking after you may need to use this term so they can make sure you have the best advice and support to help you have a healthy pregnancy. There are some increased risks associated with having a high BMI so you should be referred to a dietitian for an assessment and advice about healthy eating and exercise. You will also have extra checks, scans and tests during pregnancy to make sure that you and your baby are keeping healthy."
+
+            },  
+            {
+                "id": "00017", 
+                "BMI": "31", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00018", 
+                "BMI": "32", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00019", 
+                "BMI": "33", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00020", 
+                "BMI": "34", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00021", 
+                "BMI": "35", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00022", 
+                "BMI": "36", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "37", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "38", 
+                "Category" : "Obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "39", 
+                "Category" : "Obese"
+
+            }, 
+            {
+                "id": "00001", 
+                "BMI": "40", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "41", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "42", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "43", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "44", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "45", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "46", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": "47", 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": 48, 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": 49, 
+                "Category" : "severely obese"
+
+            },  
+            {
+                "id": "00001", 
+                "BMI": 50, 
+                "Category" : "severely obese"
+
+            },
+        ]
+
+    }
+}
